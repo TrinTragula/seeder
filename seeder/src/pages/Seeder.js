f578487e4e2613dfc3a00b602f7107f938fddf4c
TrinTragula <danielescarinci42@gmail.com>
2021-08-30 21:55:00 +0200

    ...but I'll accept a coffee

--- seeder/src/pages/Seeder.js
+++ seeder/src/pages/Seeder.js
@@ -365,6 +365,13 @@ export default function Seeder() {
                         {buttonText}
                     </button>
                 </div>
+                <div className="flex-row margin-3">
+                    <div className="margin-3">Buy me a coffee!</div>
+                    <form className="margin-3" action="https://www.paypal.com/donate" method="post" target="_top">
+                        <input style={{ border: "0px" }} type="hidden" name="hosted_button_id" value="LNMNHMPCKH6MG" />
+                        <input style={{ verticalAlign: "bottom", border: "0px" }} type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    </form>
+                </div>
             </div>
             {renderLegend()}
         </>

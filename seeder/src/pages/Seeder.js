b7d8a0106f5be09619c6d293eafcbc6d1b939591
TrinTragula <danielescarinci42@gmail.com>
2021-08-30 21:26:00 +0200

    Revert "Test ads for website" - Too many ads and banners on the web, fuck it
    This reverts commit baa86bb3c8f510a0484719354f490be527477ed8.

--- seeder/src/pages/Seeder.js
+++ seeder/src/pages/Seeder.js
@@ -365,18 +365,6 @@ export default function Seeder() {
                         {buttonText}
                     </button>
                 </div>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2625181666337030"
-                    crossOrigin="anonymous"></script>
-                {/* <!-- Barra ricerca seed --> */}
-                <ins className="adsbygoogle"
-                    style={{ display: "block" }}
-                    data-ad-client="ca-pub-2625181666337030"
-                    data-ad-slot="2608294652"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({ });
-                </script>
             </div>
             {renderLegend()}
         </>

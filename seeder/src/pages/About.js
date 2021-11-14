4171083c45d54935b435c3206a8bce08e4cbdaf4
TrinTragula <danielescarinci42@gmail.com>
2021-11-14 16:27:30 +0100

    Own a seed in the blockchain!

--- seeder/src/pages/About.js
+++ seeder/src/pages/About.js
@@ -7,20 +7,28 @@ export default function About() {
             <div className="about-container">
                 <div className="flex-row flex-align-center">
                     <h1>Seeder</h1>
-                    <small className="margin-left-15">(0.3.1)</small>
+                    <small className="margin-left-15">(0.3.2)</small>
                 </div>
                 <div className="flex-row flex-align-center">
-                    <div className="margin-3">Buy me a coffee!</div>
+                    <div className="margin-3">
+                        <img height="25" style={{ verticalAlign: "top" }} src="svg/opensea.svg" />
+                        <a href="https://opensea.io/collection/mcseeder-collection" target="_blank">
+                            Own a seed in the blockchain!
+                        </a>
+                    </div>
+                </div>
+                <div className="flex-row flex-align-center">
+                    <div className="margin-3">Or buy me a coffee!</div>
                     <form className="margin-3" action="https://www.paypal.com/donate" method="post" target="_top">
                         <input style={{ border: "0px" }} type="hidden" name="hosted_button_id" value="LNMNHMPCKH6MG" />
                         <input style={{ verticalAlign: "bottom", border: "0px" }} type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                     </form>
                 </div>
-                <div className="flex-row flex-align-center">BTC:
-                    <small className="address"><i>bc1q7j97cnz7jce3kgf3fh7hgfvu2h9q5ywrmleh0d</i></small>
+                <div className="flex-row flex-align-center">
+                    <small className="address">BTC:<i>bc1q7j97cnz7jce3kgf3fh7hgfvu2h9q5ywrmleh0d</i></small>
                 </div>
-                <div className="flex-row flex-align-center">ETH:
-                    <small className="address"><i>0x1fDA7E78fa0894b919b08DEF22B50e404D15384a</i></small>
+                <div className="flex-row flex-align-center">
+                    <small className="address">ETH:<i>0x1fDA7E78fa0894b919b08DEF22B50e404D15384a</i></small>
                 </div>
                 <h3>Who</h3>
                 <p className="paragraph">
